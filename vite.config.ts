41002140976b0818c73dabcdb8565af3ee080439
Talsor <damien.brgs92@gmail.com>
2024-07-11 18:55:27 +0200

    road v6

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/duolingo/', // Assurez-vous que cela correspond au nom de votre dépôt GitHub
+  build: {
+    rollupOptions: {
+      external: ['/dist/assets/index-C4hwDUTq.js'], // Exemple d'externalisation
+    },
+  },
 });

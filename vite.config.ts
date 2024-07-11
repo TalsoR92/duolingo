88034c60b0c49c64bef2305cf8137ccc9bdf58c7
Talsor <damien.brgs92@gmail.com>
2024-07-11 18:23:02 +0200

    road v4

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/duolingo/', // Assurez-vous que cela correspond au nom de votre dépôt GitHub
+});
